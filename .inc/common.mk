9e11423377f6da9360fb7404d62918bbfe0f3513
grantmacken <grantmacken@gmail.com>
2020-06-12 08:05:13 +1200

    move defines here

--- .inc/common.mk
+++ .inc/common.mk
@@ -1,3 +1,17 @@
+
+define xqOrderedCompile
+xqerl:compile(\"code/src/newBase60.xqm\"),\
+xqerl:compile(\"code/src/maps.xqm\"),\
+xqerl:compile(\"code/src/req_res.xqm\"),\
+xqerl:compile(\"code/src/render.xqm\"),\
+xqerl:compile(\"code/src/posts.xqm\"),\
+xqerl:compile(\"code/src/routes.xqm\").
+endef
+
+compiledLibs := 'BinList = xqerl_code_server:library_namespaces(),\
+ NormalList = [binary_to_list(X) || X <- BinList],\
+ io:fwrite("~1p~n",[lists:sort(NormalList)]).'
+
 PROXY_IMAGE = $(PROXY_DOCKER_IMAGE):$(NGX_VER)
 XQERL_IMAGE = $(XQERL_DOCKER_IMAGE):$(XQ_VER)
 XQ := $(XQERL_CONTAINER_NAME)
@@ -27,7 +41,33 @@ MountNginxConf   := type=volume,target=$(PROXY_HOME)/conf,source=nginx-configura
 MountLetsencrypt := type=volume,target=$(LETSENCRYPT),source=letsencrypt
 # bind mount might make volume for escripts
 # MountBin     := type=bind,target=$(XQERL_HOME)/bin/scripts,source=$(CURDIR)/bin
-# pretty print
+
+define xqRun
+ docker run --rm  \
+ --mount $(MountCode) \
+ --mount $(MountData) \
+ --mount $(MountEscripts) \
+ --name  $(XQ) \
+ --hostname xqerl \
+ --network $(NETWORK) \
+ --publish $(XQERL_PORT):$(XQERL_PORT) \
+ --detach \
+ $(XQERL_IMAGE)
+endef
+
+define ngxRun
+docker run --rm \
+ --mount $(MountNginxConf) \
+ --mount $(MountAssets) \
+ --mount $(MountLetsencrypt) \
+ --name  $(PROXY_CONTAINER_NAME) \
+ --hostname nginx \
+ --network $(NETWORK) \
+ --publish 80:80 \
+ --publish 443:443 \
+ --detach \
+ $(PROXY_IMAGE)
+endef
 
 Tick  = echo -n "$$(tput setaf 2) ✔ $$(tput sgr0) " && echo -n $1
 Cross = echo -n "$$(tput setaf 1) ✘ $$(tput sgr0) " && echo -n $1
@@ -37,3 +77,5 @@ containerRunning = docker ps -q --all --filter name=$1 --format '$1: [ {{.Status
 
 MustHaveVolume = docker volume list --format "{{.Name}}" | \
  grep -q $(1) || docker volume create --driver local --name $(1) &>/dev/null
+
+
