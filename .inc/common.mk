9a2b0a06b67673837ae211c0a809668eb7203447
grantmacken <grantmacken@gmail.com>
2020-06-23 12:19:17 +1200

    rename

--- .inc/common.mk
+++ .inc/common.mk
@@ -73,7 +73,7 @@ Tick  = echo -n "$$(tput setaf 2) ✔ $$(tput sgr0) " && echo -n $1
 Cross = echo -n "$$(tput setaf 1) ✘ $$(tput sgr0) " && echo -n $1
 
 # function usage: $(if $(call containerRunning,$(XQ)),yep,nope)
-containerRunning = docker ps -q --all --filter name=$1 --format '$1: [ {{.Status}} ]'
+containerRunning = docker ps -q --all --filter name=$1 --format '$1: [ {{.Status}} ]' 2>/dev/null || true
 
 MustHaveVolume = docker volume list --format "{{.Name}}" | \
  grep -q $(1) || docker volume create --driver local --name $(1) &>/dev/null
